3a8e2ba715943712549df40dcdb78b78068ebe80
Kevin Gmelin <kgmelin11@gmail.com>
2022-12-13 23:04:21 +0000

    Halve the size of semantic stream

--- models/SemanticStream.py
+++ models/SemanticStream.py
@@ -27,34 +27,30 @@ class SemanticStream(nn.Module):
         self.fuse_lat_0 = nn.Conv2d(512+1024, 512, 1)
         self.fuse_lat_1 = nn.Conv2d(256+512, 256, 1)
         self.fuse_lat_2 = nn.Conv2d(128+256, 128, 1)
-        self.fuse_lat_3 = nn.Conv2d(128+128, 128, 1)
-        self.fuse_lat_4 = nn.Conv2d(64+64, 64, 1)
-        self.fuse_lat_5 = nn.Conv2d(32+32, 32, 1)
+        self.fuse_lat_3 = nn.Conv2d(128+64, 64, 1)
+        self.fuse_lat_4 = nn.Conv2d(64+32, 32, 1)
+        self.fuse_lat_5 = nn.Conv2d(32+16, 16, 1)
 
         self.batchnorm = True
         self.decoder4 = nn.Sequential(
-          ConvBlock(128, 128, 1, self.batchnorm),
-          ConvBlock(128, 128, 1, self.batchnorm, residual=True),
+          ConvBlock(128, 64, 1, self.batchnorm),
+          ConvBlock(64, 64, 1, self.batchnorm, residual=True),
           nn.UpsamplingBilinear2d(scale_factor=2)
         )
 
         self.decoder5 = nn.Sequential(
-            ConvBlock(128, 64, 1, self.batchnorm),
-            ConvBlock(64, 64, 1, self.batchnorm, residual=True),
-            nn.UpsamplingBilinear2d(scale_factor=2)
-        )
-
-        self.decoder6 = nn.Sequential(
             ConvBlock(64, 32, 1, self.batchnorm),
             ConvBlock(32, 32, 1, self.batchnorm, residual=True),
             nn.UpsamplingBilinear2d(scale_factor=2)
         )
-        
-        # 1x conv & identity & downsample
-        self.down = nn.Sequential(
-            ConvBlock(32, 16, 1, self.batchnorm, self.channels_out),
-            ConvBlock(self.channels_out, 16, 1, self.batchnorm, self.channels_out, residual=True),
+
+        self.decoder6 = nn.Sequential(
+            ConvBlock(32, 16, 1, self.batchnorm),
+            ConvBlock(16, 16, 1, self.batchnorm, residual=True),
+            nn.UpsamplingBilinear2d(scale_factor=2)
         )
+        
+        self.down = nn.Conv2d(16, self.channels_out, kernel_size=1)
     
     def forward(self, rgb_image, language_command, lateral_outs):
         sentence_embedding = self.clip_model.embed_sentence(language_command)
@@ -75,14 +71,14 @@ class SemanticStream(nn.Module):
         x = x * lang_tile1
         x = self.up_block1(x, layer3)
 
-        x = x * lang_tile2
         x = torch.cat([x, lateral_outs[0]], dim=1)
         x = self.fuse_lat_0(x)
+        x = x * lang_tile2
         x = self.up_block2(x, layer2)
 
-        x = x * lang_tile3
         x = torch.cat([x, lateral_outs[1]], dim=1)
         x = self.fuse_lat_1(x)
+        x = x * lang_tile3
         x = self.up_block3(x, layer1)
 
         x = torch.cat([x, lateral_outs[2]], dim=1)
